e6eb2880afd018c5fe7391e6cba6df2c1737b404
JackR42 <j.roumimper@gmail.com>
2022-08-19 19:33:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,20 +29,17 @@ resource "azurerm_mssql_server" "example" {
 resource "azurerm_mssql_database" "example" {
   name                = "dba"
   resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
   server_id         = azurerm_mssql_server.example.id
 }
 
 resource "azurerm_mssql_firewall_rule" "example1" {
   name                = "FirewallRule1"
-  resource_group_name = azurerm_resource_group.example.name
   server_id           = azurerm_mssql_server.example.id
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
 resource "azurerm_mssql_firewall_rule" "example2" {
   name                = "FirewallRule2"
-  resource_group_name = azurerm_resource_group.example.name
   server_id           = azurerm_mssql_server.example.id
   start_ip_address    = "94.209.108.55"
   end_ip_address      = "94.209.108.55"
