d7b8a8a0f60b861af19009eecb6d29e47b9c6788
JackR42 <j.roumimper@gmail.com>
2022-08-19 18:58:57 +0200

    Update dba.sql

--- dba.sql
+++ dba.sql
@@ -1,19 +1,22 @@
-create table t1(
+create schema s42
+
+create table s42.t42(
   id int primary key not null
   , description char(32) null
- )
- insert into t1 values(42, '42')
- insert into t1 values(911, '911')
- insert into t1 values(1433, '1433')
+)
+insert into s42.t42 values(42, '42')
+insert into s42.t42 values(911, '911')
+insert into s42.t42 values(1433, '1433')
  
- create view v1 as select * from t1
+create view s42.v42 as select * from t1
  
- create function f1()
- returns table
- as
- return select * from v1
+create function s42.f42()
+returns table
+as
+return select * from s42.v42
  
- create procedure p1 
- as
- select * from f1
+create procedure s42.p42 
+as
+select * from f1
  
+execute s42.p42
