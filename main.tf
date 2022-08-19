c3bea1627254f9555a2173ed867bd5c7dfff2299
JackR42 <j.roumimper@gmail.com>
2022-08-19 18:19:56 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+# main.tf
+
+resource "azurerm_resource_group" "example" {
+  name     = "S2-RG-Project06"
+  location = "West Europe"
+}
+
+resource "azurerm_sql_server" "example" {
+  name                         = "sqlproject06421433911"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "admindba"
+  administrator_login_password = "ABCabc123.42"
+
+  tags = {
+    env = "dev"
+  }
